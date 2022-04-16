1424d1595787754ff4e81ea9405f683f9f34ccbb
AnuarGarnica <54009406+AnuarGarnica@users.noreply.github.com>
2022-04-16 01:49:39 -0500

    Se agrega la aplicacion

---
+++ AdministracionHoteles/AdministracionHoteles.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministracionHoteles.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
